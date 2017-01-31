e8f22e51c0f71d573e63fddbc2920f11f5eb3fd0
janikibichi <janikibichi@gmail.com>
2017-01-31 16:53:34 +0300

    ussd-rb done

---
+++ ussd-rb/ussd.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require './AfricasTalkingGateway'
+require 'dotenv'
+Dotenv.load
+
+#instantiate gateway
+gateway = AfricasTalkingGateway.new(ENV['AT_API_USERNAME'], ENV['AT_API_KEY'],"sandbox")
+
+post '/ussd' do
+ @sessionId = params[:sessionId]
+ @serviceCode = params[:serviceCode]
+ @phoneNumber = params[:phoneNumber]
+ @text = params[:text]
+
+ puts "text -#{@text} on - #{@phoneNumber}"
+
+ if( @text == "" )
+  # This is the first request. Note how we start the response with CON
+  response  = "CON What would you like to do? \n"
+  response += "1. is the value of happiness \n"
+  response += "2. is the formula of laughing"
+
+ elsif(@text=="1")
+  # Your business logic to determine the account number goes here
+  accountNumber  = "ACC1001"
+  # This is a terminal request. Note how we start the response with END
+  response = "END Your account number is $accountNumber"
+
+ elsif(@text =="2")
+  # Your business logic to determine the balance goes here
+  balance  = "KES 1,000"
+  # This is a terminal request. Note how we start the response with END
+  response = "END Your balance is $balance"
+
+ end
+ # Print the response onto the page so that our gateway can read it
+ body response
+ status 200
+end
