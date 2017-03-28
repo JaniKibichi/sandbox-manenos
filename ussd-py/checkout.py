3e6a0597470390d36116861531e845a90734c094
janikibichi <janikibichi@gmail.com>
2017-03-28 16:37:32 +0300

    py3

--- ussd-py/checkout.py
+++ ussd-py/checkout.py
@@ -2,5 +2,14 @@
 from africastalking.AfricasTalkingGateway import (AfricasTalkingGateway, AfricasTalkingGatewayException)
 #create a gateway instance
 gateway = AfricasTalkingGateway("jani","d46192b5e6c1bdf6e24ae3760f5d49cde42e8b09d53f01fc929eec205996f5ce","sandbox")
-#send airtime
-gateway.sendAirtime([{"phoneNumber":"+254708415904", "amount":"KES 20" }])
+
+productName  = "Good Morning"
+phoneNumber  = "+254708415904"
+currencyCode = "KES"
+amount       = 100.50
+metadata     = {"agentId"   : "654", "productId" : "321"}
+
+gateway.initiateMobilePaymentCheckout(productName,phoneNumber,currencyCode,amount,metadata)
+
+
+
--- ussd-py/sms.py
+++ ussd-py/sms.py
@@ -1,5 +1,6 @@
-from AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
+from africastalking.AfricasTalkingGateway import (AfricasTalkingGateway, AfricasTalkingGatewayException)
+
 #create a gateway instance
-gateway = AfricasTalkingGateway("jani","e880c35360e9fba107c480365f3094eb836e01167096495bc4e4b0d6c71058ee","sandbox")
+gateway = AfricasTalkingGateway("jani","d46192b5e6c1bdf6e24ae3760f5d49cde42e8b09d53f01fc929eec205996f5ce","sandbox")
 #send message
-gateway.sendMessage("+254787235065","Hello......")
+gateway.sendMessage("+254708415904","Hello......")
